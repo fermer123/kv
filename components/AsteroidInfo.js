7bd65348fa200d697a52803fb4f6ff171cad08fc
fermer123 <fermer_525@mail.ru>
2022-08-14 18:53:07 +0500

    btn

--- components/AsteroidInfo.js
+++ components/AsteroidInfo.js
@@ -1,4 +1,3 @@
-import wrapper from '../styles/Home.module.scss';
 import style from '../styles/AsteroidInfo.module.scss';
 import Image from 'next/image';
 import danger from '../public/danger.jpg';
@@ -12,39 +11,44 @@ const AsteroidInfo = ({ state }) => {
     is_potentially_hazardous_asteroid,
   } = state;
 
-  const { close_approach_date } = close_approach_data[0];
+  const { close_approach_date, miss_distance } = close_approach_data[0];
   const { estimated_diameter_max } = estimated_diameter.meters;
+  const { kilometers, lunar } = miss_distance;
+  console.log(lunar);
 
   return (
-    <div className={wrapper.wrapper}>
-      <div className={style.AsteroidInfo}>
-        <div className={style.AsteroidInfo_date}>{close_approach_date}</div>
-        <div className={style.AsteroidInfo_body}>
-          <div className={style.AsteroidInfo_body_image}>
-            {is_potentially_hazardous_asteroid ? (
-              <Image
-                src={danger}
-                width={93}
-                height={95}
-                placeholder='blur'
-                alt='danger'
-              />
-            ) : (
-              <Image src={podlet} placeholder='blur' alt='podlet' />
-            )}
-          </div>
-          <div className={style.AsteroidInfo_body_info}>
-            <div className={style.AsteroidInfo_body_name}>{name}</div>
-            <div className={style.AsteroidInfo_body_size}>
-              Ø {Math.ceil(estimated_diameter_max)} м
-            </div>
-            <div className={style.AsteroidInfo_body_size}>↔ {name}</div>
-          </div>
+    <div className={style.AsteroidInfo}>
+      <div className={style.AsteroidInfo_date}>{close_approach_date}</div>
+      <div className={style.AsteroidInfo_body}>
+        <div className={style.AsteroidInfo_body_image}>
+          {is_potentially_hazardous_asteroid ? (
+            <Image
+              src={danger}
+              width={93}
+              height={95}
+              placeholder='blur'
+              alt='danger'
+            />
+          ) : (
+            <Image src={podlet} placeholder='blur' alt='podlet' />
+          )}
         </div>
+        <ul className={style.AsteroidInfo_body_info}>
+          <li className={style.AsteroidInfo_body_name}>Астеройд {name}</li>
+          <li className={style.AsteroidInfo_body_size}>
+            Ø {Math.ceil(estimated_diameter_max)} м
+          </li>
+          <li className={style.AsteroidInfo_body_size}>↔ {name}</li>
+          <li className={style.AsteroidInfo_body_danger}>
+            {is_potentially_hazardous_asteroid ? 'Опасен' : 'Не опасен'}
+          </li>
+        </ul>
+      </div>
+      <div className={style.AsteroidInfo_btn}>
+        <button>уничтожить</button>
       </div>
     </div>
   );
 };
 
 export default AsteroidInfo;
-//{is_potentially_hazardous_asteroid ? 'Опасен' : 'Не опасен'}
